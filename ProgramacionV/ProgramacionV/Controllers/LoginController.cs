ffc39fd021fc7a4d7ebcb6997cc8e80ac75ff651
Fabian Saladino <fabian.saladino@logtalent.com.br>
2026-05-13 16:21:49 -0300

    Proyecto base

---
+++ ProgramacionV/ProgramacionV/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using ProgramacionV.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace ProgramacionV.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(LoginViewModel model)
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                string query = @"
+                   SELECT COUNT(*)
+                   FROM Users
+                   WHERE Username = @Username
+                   AND Password = @Password";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", model.username);
+                    cmd.Parameters.AddWithValue("@Password", model.password);
+
+                    int count = (int)cmd.ExecuteScalar();
+
+                    if (count > 0)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+
+            ViewBag.Error = "Usuario o contraseña incorrectos";
+
+            return View(model);
+        }
+    }
+}
---
+++ ProgramacionV/ProgramacionV/Models/LoginViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgramacionV.Models
+{
+    public class LoginViewModel
+    {
+        public string username {  get; set; }
+        public string password { get; set; }
+    }
+}
